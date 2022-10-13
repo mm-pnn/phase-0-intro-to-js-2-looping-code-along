00e639c30a2e46f234ec1ebfdb160c9a642def68
Melanie Penn <melanie.penn17@gmail.com>
2022-10-13 18:32:49 -0500

    commit tree cb383bdef6b6e4e989404c659eaeb549ad7e5ed0

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        debugger;
+    }
+    
+    return gifts;
+}
+wrapGifts(gifts);
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards() {
+    var messages = []
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        console.log(messages[i])
+        debugger;
+    }
+    return messages;
+}
+writeCards(names);
+
+function countDown() {
+    let countdown = 0;
+    while (countdown < 11) {
+        console.log(countdown++);
+    }
+}
